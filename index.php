591781991693429feac68f4b6ece037f1d24acea
Redsmicraft03 <hanifwisanggeniprabowo6@gmail.com>
2025-02-24 09:08:19 +0700

    menambahkan paragrap

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote</title>
+</head>
+<body>
+    <h1>hello world!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit voluptatibus quos accusamus ut doloremque officiis molestiae. Corrupti maiores, molestiae ducimus odit, eligendi placeat quos porro explicabo deserunt tenetur quod deleniti.</p>
+</body>
+</html>
